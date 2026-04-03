4a93dbe4af511b830905ebf25eb9a0ac40f0c81f
Josh Burns <joshyburnss@gmail.com>
2026-04-03 22:14:13 +1100

    script to automate updating server deployment

---
+++ script/deployment_update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Downloading update"
+git pull origin main
+
+echo "Killing docker container"
+docker kill uischemes
+
+echo "Rebuilding docker container"
+docker build -t uis:latest .
+
+echo "Starting docker container"
+docker run -d -p 32769:80 --name uischemes uis:latest
